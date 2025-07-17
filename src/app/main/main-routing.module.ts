6ec8817891f27644f6901221138b683a8066d47c
Shoaibawan005 <shoaibabbas479@gmail.com>
2025-07-17 16:16:35 +0500

    add routes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
+import { ProductsComponent } from './products/products.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './users/users.component';
+import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   {
     path: '', component: MainComponent
-  }
+  },
+
+  {
+    path:'product', component : ProductsComponent
+  },
+
+  {
+    path:'order', component : OrdersComponent
+  },
+
+  {
+    path:'user', component : UsersComponent
+  },
+
+  {
+    path:'userprofile', component : ProfileComponent
+  },
+
+  {
+    path:'main', component : MainComponent
+  },
+  
 ];
 
 @NgModule({
