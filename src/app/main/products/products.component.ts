ecc25a742d10607a887bf94433f5030fc1130414
Shoaibawan005 <shoaibabbas479@gmail.com>
2025-07-18 12:08:57 +0500

    add a method to get the data

--- src/app/main/products/products.component.ts
+++ src/app/main/products/products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from '../Services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+
+    this.productService.GetAllProducts().subscribe((res: any) => {
+      console.log(res);
+    });
+      
+  }
 
 }
