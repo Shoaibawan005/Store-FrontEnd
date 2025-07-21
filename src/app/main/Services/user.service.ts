bb2bcfd74fac43b9da6818e84c0763a9adb3d11f
Shoaibawan005 <shoaibabbas479@gmail.com>
2025-07-21 15:22:07 +0500

    add some CSS and also a get method in service file

--- src/app/main/Services/user.service.ts
+++ src/app/main/Services/user.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../Models/user.model';
 // import { product } from '../Models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiURL = 'https://localhost:7068/api/Product';
+  private apiURL = 'https://localhost:7068/api/User';
 
   constructor(private http: HttpClient) { }
 
   // GetAllProducts(): Observable<product[]>{
   //   return this.http.get<product[]>(this.apiURL);
   // }
+
+  GetAllUsers(): Observable<User[]>{
+
+    return this.http.get<User[]>(this.apiURL)
+  }
 }
